332badf4f7b9f13c4cf2607b8606cbebdc895312
Sasindu Alahakoon <dilsharasasindu@gmail.com>
2025-03-15 23:58:51 +0530

    Update rule id

--- scan-command/src/test/java/io/ballerina/scan/internal/Rule012Test.java
+++ scan-command/src/test/java/io/ballerina/scan/internal/Rule012Test.java
@@ -31,12 +31,12 @@ import java.util.List;
  *
  * @since 0.1.0
  */
-public class Rule006Test extends StaticCodeAnalyzerTest {
+public class Rule012Test extends StaticCodeAnalyzerTest {
     private static final String INVALID_RANGE_EXPRESSION = "Invalid range expression";
 
     @Test(description = "test invalid range expresion")
     void testRangeOperatorAnalyzer() {
-        String documentName = "rule006_invalid_range_expression.bal";
+        String documentName = "rule012_invalid_range_expression.bal";
         Document document = loadDocument(documentName);
         ScannerContextImpl scannerContext = new ScannerContextImpl(List.of(CoreRule.INVALID_RANGE_EXPRESSION.rule()));
         StaticCodeAnalyzer staticCodeAnalyzer = new StaticCodeAnalyzer(document,
@@ -44,29 +44,29 @@ public class Rule006Test extends StaticCodeAnalyzerTest {
         staticCodeAnalyzer.analyze();
         List<Issue> issues = scannerContext.getReporter().getIssues();
         Assert.assertEquals(issues.size(), 12);
-        assertIssue(issues.get(0), documentName, 25, 21, 25, 27, "ballerina:10", 10,
+        assertIssue(issues.get(0), documentName, 25, 21, 25, 27, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(1), documentName, 29, 21, 29, 27, "ballerina:10", 10,
+        assertIssue(issues.get(1), documentName, 29, 21, 29, 27, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(2), documentName, 33, 21, 33, 28, "ballerina:10", 10,
+        assertIssue(issues.get(2), documentName, 33, 21, 33, 28, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(3), documentName, 57, 21, 57, 27, "ballerina:10", 10,
+        assertIssue(issues.get(3), documentName, 57, 21, 57, 27, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(4), documentName, 61, 21, 61, 27, "ballerina:10", 10,
+        assertIssue(issues.get(4), documentName, 61, 21, 61, 27, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(5), documentName, 65, 21, 65, 28, "ballerina:10", 10,
+        assertIssue(issues.get(5), documentName, 65, 21, 65, 28, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(6), documentName, 69, 21, 69, 26, "ballerina:10", 10,
+        assertIssue(issues.get(6), documentName, 69, 21, 69, 26, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(7), documentName, 73, 21, 73, 28, "ballerina:10", 10,
+        assertIssue(issues.get(7), documentName, 73, 21, 73, 28, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(8), documentName, 77, 21, 77, 28, "ballerina:10", 10,
+        assertIssue(issues.get(8), documentName, 77, 21, 77, 28, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(9), documentName, 81, 22, 81, 29, "ballerina:10", 10,
+        assertIssue(issues.get(9), documentName, 81, 22, 81, 29, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(10), documentName, 83, 22, 83, 29, "ballerina:10", 10,
+        assertIssue(issues.get(10), documentName, 83, 22, 83, 29, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
-        assertIssue(issues.get(11), documentName, 87, 14, 87, 22, "ballerina:10", 10,
+        assertIssue(issues.get(11), documentName, 87, 14, 87, 22, "ballerina:12", 12,
                 INVALID_RANGE_EXPRESSION, RuleKind.CODE_SMELL);
     }
 }
